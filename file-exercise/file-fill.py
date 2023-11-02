91b24c8cdb0aac0d50c7a5c72629a99b731c3180
jmsfrancies <framann@pop-os.localdomain>
2023-11-02 17:43:10 -0500

    Modified the file with the with open function

--- file-exercise/file-fill.py
+++ file-exercise/file-fill.py
@@ -2,14 +2,15 @@
 #file fill program that requires the total number of items to add, 
 #the name of each item, and their cost.
 
+
 def main ():
-	fname = open("item-list.txt", "a")
-	fname.write(("{} {}\n").format("Item Name", "Item Cost"))
-	how_many_items = int(input("How many items are you adding?  "))
-	for i in range(how_many_items):
-		i = str(input("Name of the item:  "))
-		j = round(float(input("Cost of the item:  ")),2)	
-		fname.write(("%s   %s")%(str(i),str(j)))
-		fname.write("\n")
+	with open("item-list.txt", "w") as fname:
+		fname.write(("{0} {1}\n").format("Item Name", "Item Cost"))
+		how_many_items = int(input("How many items are you adding?  "))
+		for i in range(how_many_items):
+			i = str(input("Name of the item:  "))
+			j = round(float(input("Cost of the item:  ")),2)	
+			fname.write(("%s   %s")%(str(i),str(j)))
+			fname.write("\n")
 	fname.close()
 main()	
